cdd748bf4a9a4ee40a0775ba68d324621a58d25e
itzz-aryan-121 <122987294+itzz-aryan-121@users.noreply.github.com>
2024-09-22 00:00:54 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -91,7 +91,7 @@ export const projectsData = [
     description:
       "A real-time device tracking where we can see the live location of our device. It is very much popular nowadays for lost and theft mobile.",
     tags: ["Node.JS","Express.JS","EJS","Socketio"],
-    imageURL: DevImg ,
+    imageUrl: DevImg ,
     link: "https://realtime-device-track-1-l25r.onrender.com/"
   },
 ] as const;
